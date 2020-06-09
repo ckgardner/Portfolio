dafe28091a3422550bb954bc26af9f2ed174fe3e
cade.gardner <d00299297@dmail.dixie.edu>
2020-06-09 12:39:58 -0600

    about me page done

--- app.js
+++ app.js
@@ -1,9 +1,27 @@
 var app = new Vue({
     el: '#app',
     data: {
-        page: 'aboutMe', // home, aboutMe, myWork
+        page: 'myWork', // home, aboutMe, myWork
+
+        home_selected: true,
+        aboutMe_selected: false,
+        myWork_selected: false,
     },
     methods: {
-
+        selectHome: function(){
+            this.home_selected = true;
+            this.aboutMe_selected = false;
+            this.myWork_selected = false;
+        },
+        selectMe: function(){
+            this.home_selected = false;
+            this.aboutMe_selected = true;
+            this.myWork_selected = false;
+        },
+        selectWork: function(){
+            this.home_selected = false;
+            this.aboutMe_selected = false;
+            this.myWork_selected = true;
+        },
     },
 });
