75ba21e1611bd6ba767cf97c770f199fbc0cef50
cade.gardner <d00299297@dmail.dixie.edu>
2020-07-01 15:42:18 -0600

    updates

--- app.js
+++ app.js
@@ -1,11 +1,16 @@
 var app = new Vue({
     el: '#app',
     data: {
-        page: 'myWork', // home, aboutMe, myWork
+        page: 'aboutMe', // home, aboutMe, myWork
+        workSelection: 'all', //all, frontEnd, backEnd
 
         home_selected: true,
         aboutMe_selected: false,
         myWork_selected: false,
+
+        all_selected: true,
+        front_selected: false,
+        back_selected: false,
     },
     methods: {
         selectHome: function(){
@@ -22,6 +27,21 @@ var app = new Vue({
             this.home_selected = false;
             this.aboutMe_selected = false;
             this.myWork_selected = true;
+        },
+        selectAll: function(){
+            this.all_selected = true;
+            this.front_selected = false;
+            this.back_selected = false;
+        },
+        selectFront: function(){
+            this.all_selected = false;
+            this.front_selected = true;
+            this.back_selected = false;
+        },
+        selectBack: function(){
+            this.all_selected = false;
+            this.front_selected = false;
+            this.back_selected = true;
         },
     },
 });
