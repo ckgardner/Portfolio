f4838901e8af7476e438219b5bcea4d36920141e
cade.gardner <d00299297@dmail.dixie.edu>
2020-09-16 21:46:38 -0600

    no service worker

--- app.js
+++ app.js
@@ -1,9 +1,3 @@
-if('serviceWorker' in navigator){
-    navigator.serviceWorker.register('sw.js')
-        .then((reg) => console.log("Service Worker Registered", reg))
-        .catch((err) => console.log("Service Worker Not Registered", err));
-}
-
 var app = new Vue({
     el: '#app',
     data: {
